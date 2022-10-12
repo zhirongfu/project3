17eb082e8ea4ca238ffb8c26bf65d849a382b754
Levi <73326132+zhirongfu@users.noreply.github.com>
2022-10-11 22:01:32 -0400

    Update Post.hpp

--- Post.hpp
+++ Post.hpp
@@ -1,25 +1,26 @@
 #pragma once
 #ifndef POST_H
 #define POST_H
-class Post
+#include "Account.hpp"
+class Post //post class
 {
-private:
+private: //private members
 	std::string post_title;
 	std::string post_body;
 	std::string user_name;
 	time_t time_stamp_;
-public:
+public: //public memebers
 	Post();
-	Post(std::string, std::string,std::string);
-	void setTitle(std::string);
-	std::string getTitle()const;
-	void setBody(std::string);
-	std::string getBody()const;
-	void setUsername(std::string);
+	Post(std::string title, std::string body, std:: string username);
+	void setTitle(const std::string title); //setter	
+	std::string getTitle()const; //getter
+	void setBody(const std::string body); //setter
+	std::string getBody()const; //getter
+	void setUsername(std::string username);
 	std::string getUsername()const;
 	void setTimestamp(time_t);
-	void getTimeStamp();
-	void displayPost()const;
+	void getTimeStamp(); //getter
+	void displayPost();
 
 };
 #endif 
